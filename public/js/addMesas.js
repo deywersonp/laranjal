1a218296af59ee4e7e40ba7437bfb88b6be35633
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-09-12 19:00:42 -0300

    Adicionado mesas com JS

--- public/js/addMesas.js
+++ public/js/addMesas.js
@@ -1,61 +1,122 @@
-let place01 = document.querySelector('.table-content.place01');
+containerTablePlace = document.querySelector('.container-table-place');
 
-let inicio = 1;
-let fim = 13;
+console.log(containerTablePlace);
 
-let inicio2 = fim;
-let fim2 = fim + 11;
+function insertTables() {
+    let inicio = 1;
+    let fim = 19;
 
-function criaPrimeiraFileiraMesas() {
-    let primeiraDiv = document.createElement('div');
-    primeiraDiv.classList.add('place01-1');
+    let inicio2 = 1;
+    let howMany = 12;
 
-    place01.appendChild(primeiraDiv);
+
+    for (inicio2; inicio2 <= howMany; inicio2++) {
+        if (inicio2 === 1) {
+            id = 1
+            createTables(id, inicio, fim);
+        }
+        if (inicio2 === 2) {
+            id = 21
+            createTables(id, inicio, fim);
+        }
+        if (inicio2 === 3) {
+            id = 41
+            createTables(id, inicio, fim);
+        }
+        if (inicio2 === 4) {
+            id = 61
+            createTables(id, inicio, fim);
+        }
+        if (inicio2 === 5) {
+            id = 81
+            createTables(id, inicio, fim);
+        }
+        if (inicio2 === 6) {
+            id = 101
+            createTables(id, inicio, fim);
+        }
+        if (inicio2 === 7) {
+            id = 121
+            createTables(id, inicio, fim);
+        }
+        if (inicio2 === 8) {
+            id = 141
+            createTables(id, inicio, fim);
+        }
+        if (inicio2 === 9) {
+            id = 161
+            createTables(id, inicio, fim);
+        }
+        if (inicio2 === 10) {
+            id = 181
+            createTables(id, inicio, fim);
+        }
+        if (inicio2 === 11) {
+            id = 201
+            createTables(id, inicio, fim);
+        }
+        if (inicio2 === 12) {
+            id = 221
+            createTables(id, inicio, fim);
+        }
+    }
+}
+
+function createTables(id, inicio, fim) {
+    let firstGroup = document.createElement('div');
+    firstGroup.classList.add('table-place')
+
+    let secondGroup = document.createElement('div');
+    secondGroup.classList.add('table-place')
+    secondGroup.classList.add('second')
+
+    let whoReceive = firstGroup;
+    containerTablePlace.appendChild(firstGroup)
 
     for (inicio; inicio <= fim; inicio++) {
         if (inicio % 2 === 0) {
             let img = document.createElement('img');
-            img.src = './public/images/cancel.svg';
+            img.src = '../images/cancel.svg';
             img.alt = 'Bloqueado';
             img.classList.add('table-unavailable');
-            primeiraDiv.appendChild(img);
-        } else if (inicio === fim) {
+            whoReceive.appendChild(img);
+            id++;
+
+        } else if (inicio === 11) {
             let div = document.createElement('div');
             div.classList.add('table-separator')
-            place01.appendChild(div);
+            containerTablePlace.appendChild(div);
+
+            containerTablePlace.appendChild(secondGroup);
+            whoReceive = secondGroup;
+
+            let img1 = document.createElement('img');
+            img1.src = '../images/cancel.svg';
+            img1.alt = 'Bloqueado';
+            img1.classList.add('table-unavailable');
+            whoReceive.appendChild(img1);
+            id++;
+
+            let img2 = document.createElement('img');
+            img2.src = '../images/green-circle.svg';
+            img2.alt = 'Disponível';
+            img2.classList.add('table-available');
+            img2.id = `Mesa-${id}`
+            whoReceive.appendChild(img2);
+            id++;
+
         } else {
             let img = document.createElement('img');
-            img.src = './public/images/green-circle.svg';
+            img.src = '../images/green-circle.svg';
             img.alt = 'Disponível';
             img.classList.add('table-available');
-            img.id = `Mesa-A${inicio}`
-            primeiraDiv.appendChild(img);
+            img.id = `Mesa-${id}`
+            whoReceive.appendChild(img);
+            id++;
         }
     }
 
-    criaSegundaFileiraMesas();
-}
+    return id;
+};
 
-function criaSegundaFileiraMesas() {
-    let segundaDiv = document.createElement('div');
-    segundaDiv.classList.add('place01-2');
-    place01.appendChild(segundaDiv);
-
-    for (inicio2; inicio2 <= fim2; inicio2++)
-        if (inicio2 % 2 === 1) {
-            let img = document.createElement('img');
-            img.src = './public/images/cancel.svg';
-            img.alt = 'Bloqueado';
-            img.classList.add('table-unavailable');
-            segundaDiv.appendChild(img);
-        } else {
-            let img = document.createElement('img');
-            img.src = './public/images/green-circle.svg';
-            img.alt = 'Disponível';
-            img.classList.add('table-available');
-            img.id = `Mesa-A${inicio2}`
-            segundaDiv.appendChild(img);
-        }
-}
-
-criaPrimeiraFileiraMesas();
+insertTables();
