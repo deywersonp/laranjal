549b5cd490122b3649c1e16661d472af6057227f
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-09-12 22:28:18 -0300

    Criado interação entre os modais

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,7 +1,14 @@
 let modal = document.querySelector('div.modal-wrapper');
-let button = document.querySelector('button.modal-close-button')
-let tables = document.querySelectorAll('.table-available')
-let modalInputTable = document.querySelector('input#modal-input-table')
+let closeButton = document.querySelector('button.modal-close-button');
+let continueButton = document.querySelector('button.modal-continue-button');
+let modalInputTable = document.querySelector('input#modal-input-table');
+
+let guidelineModal = document.querySelector('div.modal-wrapper-guidelines');
+let backButton = document.querySelector('div.modal-guidelines-header .back');
+let guidelineInputTable = document.querySelector('input#modal-guidelines-input-table');
+
+let tables = document.querySelectorAll('.table-available');
+
 
 document.addEventListener('DOMContentLoaded', () => {
   tables.forEach((table) => {
@@ -9,18 +16,30 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-button.addEventListener('click', closeModal)
+closeButton.addEventListener('click', closeModal);
+continueButton.addEventListener('click', openGuidelineModal);
+backButton.addEventListener('click', closeGuidelineModal);
 
 function getId(event) {
   let tableId = event.currentTarget.id
   modalInputTable.value = tableId;
+  guidelineInputTable.value = tableId;
   openModal();
-}
+};
 
 function openModal() {
   modal.classList.add('active');
-}
+};
 
 function closeModal() {
   modal.classList.remove('active');
-}
+};
+
+function openGuidelineModal() {
+  guidelineModal.classList.add('active');
+};
+
+function closeGuidelineModal() {
+  closeModal();
+  guidelineModal.classList.remove('active');
+};
