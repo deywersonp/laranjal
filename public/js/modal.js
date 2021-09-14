6b50972f0e7febfd3abfba030e8f3c3a1f9b92d9
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-09-13 22:33:46 -0300

    Finalizado interações da página de agendamentos

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,6 +1,9 @@
+//Document Variables
+let tables = document.querySelectorAll('.table-available');
 let unityInfo = document.querySelector('input.input-info1');
 let dateInfo = document.querySelector('input.input-info2');
 
+// Selected Table Modal Variables
 let modal = document.querySelector('div.modal-wrapper');
 let closeButton = document.querySelector('button.modal-close-button');
 let continueButton = document.querySelector('button.modal-continue-button');
@@ -8,11 +11,12 @@ let modalInputTable = document.querySelector('input#modal-input-table');
 let modalInputUnity = document.querySelector('input#modal-input-unity');
 let modalInputDate = document.querySelector('input#modal-input-date');
 
+// Guideline Modal Variables
 let guidelineModal = document.querySelector('div.modal-wrapper-guidelines');
 let backButton = document.querySelector('div.modal-guidelines-header .back');
 let guidelineInputTable = document.querySelector('input#modal-guidelines-input-table');
-
-let tables = document.querySelectorAll('.table-available');
+let guidelineInputUnity = document.querySelector('input#modal-guidelines-input-unity');
+let guidelineInputDate = document.querySelector('input#modal-guidelines-input-date');
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -22,6 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 closeButton.addEventListener('click', closeModal);
+
 continueButton.addEventListener('click', openGuidelineModal);
 backButton.addEventListener('click', closeGuidelineModal);
 
@@ -30,8 +35,10 @@ function getInformation(event) {
   modalInputTable.value = tableId;
   modalInputUnity.value = unityInfo.value;
   modalInputDate.value = dateInfo.value;
-  guidelineInputTable.value = tableId;
 
+  guidelineInputTable.value = tableId;
+  guidelineInputUnity.value = unityInfo.value
+  guidelineInputDate.value = dateInfo.value;
 
   openModal();
 };
@@ -45,6 +52,7 @@ function closeModal() {
 };
 
 function openGuidelineModal() {
+
   guidelineModal.classList.add('active');
 };
 
