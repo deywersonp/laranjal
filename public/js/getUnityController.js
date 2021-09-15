96bd98707527761a9c4b1fabe6a1a73a01c24c54
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-09-14 21:47:02 -0300

    Adicionado novas imagens / adicionado show on/off do botão continuar

--- public/js/getUnityController.js
+++ public/js/getUnityController.js
@@ -1,11 +1,25 @@
 let units = document.querySelectorAll("div.unity");
-console.log(units);
+let continueButton = document.querySelector("div.continuar a");
 
 document.addEventListener('DOMContentLoaded', () => {
+  localStorage.removeItem('unity');
+  continueButton.classList.remove('active');
+
   units.forEach((unity) => {
     unity.addEventListener('click', () => {
       let unitySelected = unity.children[1].value;
       localStorage.setItem('unity', unitySelected)
     });
   });
+
+  units.forEach((unity) => {
+    unity.addEventListener('click', () => {
+      if (localStorage.getItem('unity') != null) {
+        continueButton.classList.add('active');
+      }
+    });
+  });
 });
+
+
+
