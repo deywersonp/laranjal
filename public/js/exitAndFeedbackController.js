5b7ab086f04b5e02d7fd014ce99be3481b2e4cfb
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-09-15 01:38:28 -0300

    Criado script de controle de modais

---
+++ public/js/exitAndFeedbackController.js
@@ -0,0 +1,36 @@
+let feedback = document.querySelector('a.feedback');
+console.log(feedback)
+
+let feedbackModal = document.querySelector('div.feedback-modal-wrapper');
+let feedbackModalButton = document.querySelector('a.feedback-access-button');
+
+let exit = document.querySelector('a.exit');
+console.log(exit)
+
+let exitModal = document.querySelector('div.exit-modal-wrapper');
+let exitModalConfirmButton = document.querySelector('div .exit-confirm-button');
+let exitModalCloseButton = document.querySelector('div .exit-close-button');
+console.log(exitModal)
+console.log(exitModalCloseButton)
+
+
+feedback.addEventListener('click', () => {
+  feedbackModal.classList.add('active');
+});
+
+feedbackModalButton.addEventListener('click', () => {
+  feedbackModal.classList.remove('active');
+});
+
+
+exit.addEventListener('click', () => {
+  exitModal.classList.add('active');
+});
+
+exitModalConfirmButton.addEventListener('click', () => {
+  exitModal.classList.remove('active');
+});
+
+exitModalCloseButton.addEventListener('click', () => {
+  exitModal.classList.remove('active');
+});
